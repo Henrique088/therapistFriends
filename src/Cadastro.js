201938918cd4570f96c3fe90357b45bb7da0799c
Henrique <henriqueribeiro88bb@gmail.com>
2024-10-03 18:26:28 -0300

    ajustando o layout e acrescentando footer

--- src/Cadastro.js
+++ src/Cadastro.js
@@ -1,12 +1,14 @@
-import lobo from './lobo.png';
+import lobo from './img/lobo.png';
 import cadeado from './cadeado.svg';
 import styles from './App.module.css';
+import { FcGoogle } from "react-icons/fc";
+import { Link } from 'react-router-dom';
 
 
 
-function Cad(){
-    return(
-        <div className={styles.app}>
+function Cad() {
+  return (
+    <div className={styles.app}>
 
       <div className={styles.left}>
         <h1 className={styles.title}>Crie sua conta</h1>
@@ -22,41 +24,41 @@ function Cad(){
           <input placeholder="Digite seu E-mail"></input>
 
           <div className={styles.senha}>
-            <img src={cadeado} width="12px" heigth="12px"></img>
+            <img src={cadeado} width="12px" heigth="12px" alt='imagem de um lobo'></img>
             <span className="s">Senha</span>
           </div>
-        
+
           <input placeholder="Digite sua senha"></input>
 
         </div>
-        
+
         <div className={styles.botao}>
           <button className={styles.logar}>Criar conta</button>
-      
-          
-            
-          <button className={styles.logar_google}> Cadastre-se com o Google</button>
-          
+
+
+
+          <button className={styles.logar_google}><FcGoogle />{'\u00A0'} Cadastre-se com o Google</button>
+
 
         </div>
 
-      
+
 
       </div>
 
-      <div className="right">
+      <div className={styles.right}>
 
-        <img src={lobo} width="331" heigth="440" className="lobo"></img>
+        <img src={lobo} width="80%" heigth="60%" className="lobo" alt='imagem de um lobo'></img>
 
-        <span>Ainda não possui cadastro? <a href="#">Cadastre-se</a></span>
+        <span>Já possui cadastro? <Link to="/login">Logar</Link></span>
 
       </div>
-      
+
 
     </div>
 
 
-    );
+  );
 }
 
 
--- src/inicio.js
+++ src/inicio.js
@@ -1,87 +1,128 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import styles from'./nav.module.css';
-import lobo from './lobo.png';
-import flor from './flor_02.png';
+import styles from './nav.module.css';
+import lobo from './img/lobo.png';
+import flor from './img/flor_02.png';
+import leitura from './img/garota-de-vista-lateral-na-biblioteca.jpg';
+import contato from './img/contato.jpg';
+import agenda from './img/agenda.jpg';
+import maos from './img/maos.webp';
+import { FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { FaSquareInstagram } from "react-icons/fa6";
 import { LoremIpsum, Avatar } from 'react-lorem-ipsum';
-import img_card from './3918491.jpg';
+import img_card from './img/3918491.jpg';
 
 
-function Ini(){
-    const[showOptions, setShowOptions] = useState(false);
+function Ini() {
+    const [showOptions, setShowOptions] = useState(false);
 
-    const handleButtonClick = () =>{
+    const handleButtonClick = () => {
         setShowOptions(!showOptions);
     }
-    return(
+    return (
 
         <header className={styles.cabecalho}>
             <div className={styles.topo}>
-            <div className={styles.logoNome}>
-                <img src={lobo} width="10%" height="10%"></img>
-                <span>Therapist Friend</span>
-            </div>
+                <div className={styles.head}>
+                    <div className={styles.logoNome}>
 
-            <div className={styles.loginCad}>
-                <Link to="/login" className={styles.log}><a>Login</a></Link>
-            
-                <div className={styles.navButton}>
-                    <button onClick={handleButtonClick} className={styles.botao}>Novo?</button>
-                    {showOptions && (
-                        <div className={styles.options}>
-                            <Link to="/cadastro">Paciente</Link>
-                            <Link to="/cadastro"> Profissional</Link>
+                        <Link to="/login" className={styles.img_topo}><img src={lobo} width="40px" alt='imagem de um lobo'></img> Therapist Friend</Link>
+
+                    </div>
+
+                    <div className={styles.loginCad}>
+                        <Link to="/login" className={styles.log}><a>Login</a></Link>
+
+                        <div className={styles.navButton}>
+                            <button onClick={handleButtonClick} className={styles.botao}>Novo?</button>
+                            {showOptions && (
+                                <div className={styles.options}>
+                                    <Link to="/cadastro">Paciente</Link>
+                                    <Link to="/cadastro"> Profissional</Link>
+                                </div>
+
+                            )}
                         </div>
-
-                    )}
+                    </div>
                 </div>
-            </div>
-            <hr></hr>
+                {/* <hr></hr> */}
             </div>
 
 
             <div className={styles.conteudo}>
                 <div className={styles.bemVindo}>
-                    <img src={flor} width="20%"></img>
+                    <img src={flor} className={styles.img}></img>
                     <span className={styles.texto}>
-                    Bem-vindo(a) à Therapist Friend, um espaço dedicado a oferecer suporte emocional e psicológico para aqueles que mais necessitam. Nossa missão é reduzir as taxas de evasão e a falta de procura por atendimento psicológico, proporcionando um ambiente seguro e anônimo onde os usuários podem desabafar e encontrar o apoio necessário.
+                        Bem-vindo(a) à Therapist Friend, um espaço dedicado a oferecer suporte emocional e psicológico para aqueles que mais necessitam. Nossa missão é reduzir as taxas de evasão e a falta de procura por atendimento psicológico, proporcionando um ambiente seguro e anônimo onde os usuários podem desabafar e encontrar o apoio necessário.
 
 
                     </span>
                 </div>
-            </div>        
-                <span className={styles.comoFunciona}>Como funciona?</span>
-                
-                <div className={styles.cards}>
-                    
-                    <div className={styles.a}>
-                        <img src={img_card} width="25%" ></img>
-                        <span className={styles.b}>1. Desabafe Anonimamente: Usuários podem compartilhar suas preocupações, angústias e desafios de forma completamente anônima na nossa plataforma. Este espaço de desabafo é confidencial e protegido, garantindo que você possa expressar seus sentimentos sem medo de julgamento.</span>
-                    </div>
-
-                    <div className={styles.a}>
-                        <img src={img_card} width="25%"></img>
-                        <span className={styles.b}>2. Leitura pelos Profissionais: Terapeutas e psicólogos cadastrados na plataforma terão acesso às mensagens anônimas dos usuários. Eles podem ler e avaliar cada caso de acordo com sua especialização e experiência.</span>
-                    </div>
+            </div>
+            <span className={styles.comoFunciona}>Como funciona?</span>
 
 
-                    <div className={styles.a}>
-                        <img src={img_card} width="25%" ></img>
-                        <span className={styles.b}>3. Contato Iniciado pelos Profissionais: Se um profissional se sentir apto a ajudar, ele pode iniciar um contato com o usuário através de mensagens dentro da plataforma. Durante essa interação, o usuário terá acesso ao perfil do profissional, incluindo suas qualificações, experiência e avaliações de outros pacientes, proporcionando credibilidade e confiança na conversa.</span>
-                    </div>
+            <div className={styles.f}>
+                <div class={styles.card}>
+                    <a> Desabafe Anonimamente</a>
 
-                    <div className={styles.a}>
-                        <img src={img_card} width="25%" ></img>
-                        <span className={styles.b}>4. Agendamento de Atendimento: Caso o usuário e o profissional concordem, é possível marcar uma consulta, que pode ser realizada online ou presencialmente, conforme a preferência e disponibilidade de ambos.</span>
+                    <div class={styles.card__content} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${maos})` }}>
+
+                        <p class={styles.card__title}>Desabafe</p>
+                        <p class={styles.card__description}>
+                            Usuários podem compartilhar suas preocupações, angústias e desafios de forma completamente anônima na nossa plataforma. Este espaço de desabafo é confidencial e protegido, garantindo que você possa expressar seus sentimentos sem medo de julgamento.
+                        </p>
                     </div>
-                    
-                    
-                    
                 </div>
 
-            
+                <div class={styles.card} >
+                    <a> Leitura Pelos Profissionais</a>
+
+                    <div class={styles.card__content} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${leitura})` }}>
+                        <p class={styles.card__title}>Profissionais</p>
+                        <p class={styles.card__description}>
+                            Terapeutas e psicólogos cadastrados na plataforma terão acesso às mensagens anônimas dos usuários. Eles podem ler e avaliar cada caso de acordo com sua especialização e experiência.
+                        </p>
+                    </div>
+                </div>
+
+                <div class={styles.card}>
+                    <a> Contato Iniciado pelos Profissionais</a>
+
+                    <div class={styles.card__content} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${contato})` }}>
+
+                        <p class={styles.card__title}>Contato</p>
+                        <p class={styles.card__description}>
+                            Se um profissional se sentir apto a ajudar, ele pode iniciar um contato com o usuário através de mensagens dentro da plataforma. Durante essa interação, o usuário terá acesso ao perfil do profissional, incluindo suas qualificações, experiência e avaliações de outros pacientes, proporcionando credibilidade e confiança na conversa.
+                        </p>
+                    </div>
+                </div>
+
+                <div class={styles.card}>
+                    <a> Agendamento de Atendimento</a>
+
+                    <div class={styles.card__content} style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${agenda})` }}>
+                        <p class={styles.card__title}>Agendamento</p>
+                        <p class={styles.card__description}>
+                            Caso o usuário e o profissional concordem, é possível marcar uma consulta, que pode ser realizada online ou presencialmente, conforme a preferência e disponibilidade de ambos.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                <a className={styles.criador}>Henrique Ribeiro da Silva Almeida</a>
+                <div className={styles.contato}>
+                    <p>Contatos:</p>
+                    <a href='#'><SiGmail />{'\u00A0'} E-mail</a>
+                    <a href='#'><FaLinkedin /> {'\u00A0'} Linkedin</a>
+                    <a href='#'><FaSquareInstagram /> {'\u00A0'} Instagram</a>
+                </div>
+            </footer>
+
         </header>
-        
+
 
 
     );
